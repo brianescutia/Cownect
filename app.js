df29179b5aa2f39cdc63fd808f13c672f5f4f83b
Brian Escutia <bescutia@ucdavis.edu>
2025-06-29 17:51:25 -0700

    Fix folder name, remove old index.html, add new landing page files

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const path = require('path'); // Built-in Node module
 const app = express();
 const port = 3000;
 
+// Serve static files like app.js
+app.use(express.static('public'));
+
+// Serve the HTML file
 app.get('/', (req, res) => {
   res.send('Hello, World!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
