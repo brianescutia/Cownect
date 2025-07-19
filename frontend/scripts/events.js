f5e2fd52b1d8b9af613ce539a51e8ea3a33f720a
yashgunde <yagunde@ucdavis.edu>
2025-07-18 19:00:26 -0700

    Added Calendar To Events page
    Still not dynamic, Planning on Using MongoDB for dynamic event loading and calendar updating

--- frontend/scripts/events.js
+++ frontend/scripts/events.js
@@ -7,15 +7,37 @@ class EventsCalendar {
         this.currentDate = new Date();
         this.events = {
             //Example for now must replace with API data in future
-            '2023-10-01': [{
-                id: 1,
-                title: 'HackDavis Hackathon',
-                time: '12:00 PM - 3:00 PM',
-            }
-
+            '2025-09-10': [
+                {
+                    id: 1,
+                    title: 'HackDavis Hackathon',
+                    time: '12:00pm – 3:00pm',
+                    location: 'TLC 3203',
+                    description: 'Join UC Davis\'s premier hackathon focused on social good!'
+                }
             ],
-        };
+            '2025-10-22': [
+                {
+                    id: 2,
+                    title: 'Tech Career Fair',
+                    time: '10:00am – 1:00pm',
+                    location: 'UC Center',
+                    description: 'Explore internships and job opportunities at UC Davis\'s Tech Career Fair.'
+                }
+            ],
+            '2025-12-07': [
+                {
+                    id: 3,
+                    title: 'UC Davis Research Expo',
+                    time: '10:00am – 3:00pm',
+                    location: 'UC Center',
+                    description: 'Showcasing insight and resources to help researchers succeed.'
+                }
+            ]
+        }
+        this.selectedDate = null;
     }
+    //In progress 
 }
 //Functions 
 async function checkAuthentication() {
